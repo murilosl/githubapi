9cce246d7c2caff91746726b10a93f8929946f80
Murilo de Souza Lopes <murilosl@gmail.com>
2019-08-02 15:50:35 -0300

    project base

---
+++ Github App/Github App/Resources/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Clean Swift Template Project
+//
+//  Created by Murilo de Souza Lopes on 02/08/19.
+//  Copyright © 2019 Murilo de Souza Lopes. All rights reserved.
+//
+
+import Foundation
+
+class Constants{
+   static let base_url = "https://api.github.com/search/repositories?q=language:swift&sort=stars"
+}
